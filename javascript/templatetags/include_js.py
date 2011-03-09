36429863b15d6b11a82ee04b8dcf9be870873874
defrex <defrex@gmail.com>
2011-03-09 17:11:42 -0500

    readd

--- javascript/templatetags/include_js.py
+++ javascript/templatetags/include_js.py
@@ -13,7 +13,7 @@ register = template.Library()
 @register.simple_tag
 def include_js():
     if not settings.USE_COMPILED_JS:
-        jsc = JavaScriptCompiler(settings.JS_DIR, 
+        jsc = JavaScriptCompiler(settings.JS_DIR,
                 compiled_loc=settings.COMPILED_JS_LOC,
                 recurse=settings.INCLUDE_JS_RECURSIVELY)
         jsc.resolve_dependencies()
@@ -22,7 +22,7 @@ def include_js():
         js_files = ['/'+settings.COMPILED_JS_LOC.split(settings.MEDIA_ROOT)[-1]]
     version = JSVersion.objects.get_or_create(pk=1)[0].version
     resp = ''
-    for f in js_files: 
+    for f in js_files:
         resp += '<script src="%s?version=%s"></script>' % (
                 settings.MEDIA_URL+f[1:], str(version))
     return resp
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+
 setup(
     name="django-javascript",
     version="0.1",
